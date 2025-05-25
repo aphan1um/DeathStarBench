1b3ece29c4579abdc4fe8993c7c3806fe1567a71
aphan1um <aphan1.student.unimelb.edu.au>
2025-05-25 23:31:36 +1000

    Update folder name

---
+++ custom/k8s-scaler/setup/create-certs.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# create random key, along with CSR
+mkdir -p output
+openssl genrsa -out output/cert.key 2048
+openssl req -new -key output/cert.key -out output/cert.csr -config cert.conf -subj '/CN=k8s-scaler.scaler.svc'
+
+# create cert, signed with CA as the cert itself
+openssl x509 -req -in output/cert.csr -signkey output/cert.key -out output/cert.crt -days 3650 -extensions req_ext -extfile cert.conf
+
+echo -e '\n\n'
+cat output/cert.key | base64 -w 0
+echo -e '\n\n'
+cat output/cert.crt | base64 -w 0
+echo -e '\n\n'
