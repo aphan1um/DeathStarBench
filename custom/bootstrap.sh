e235c8e8a557905daf6734b7a6abb351b1455c0d
aphan1um <aphan1.student.unimelb.edu.au>
2025-05-26 00:30:53 +1000

    Prepare webhook to update limits of pod using configmap

---
+++ custom/bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# setup autoscaler admission webhook
+kubectl apply -f k8s-scaler/spec.yaml
+kubectl apply -f admin-rbac.yaml
+
+# create configmap storing current cpu and memory limits of microservices (scaler k8s namespace created in last step already)
+DEPLOY_JSON="$(kubectl get deploy -o json)"
+CPU_CONFIGMAP_DATA=$(echo "$DEPLOY_JSON" | \
+  jq -r '.items | map("--from-literal=" + .metadata.name + "=" + .spec.template.spec.containers[0].resources.limits.cpu) | join(" ")'
+)
+MEM_CONFIGMAP_DATA=$(echo "$DEPLOY_JSON" | \
+  jq -r '.items | map("--from-literal=" + .metadata.name + "=" + .spec.template.spec.containers[0].resources.limits.memory) | join(" ")'
+)
+
+set -x
+kubectl -n scaler delete configmap cpu-limits mem-limits || true
+kubectl -n scaler create configmap cpu-limits $CPU_CONFIGMAP_DATA
+kubectl -n scaler create configmap mem-limits $MEM_CONFIGMAP_DATA
--- custom/k8s-scaler/app/main.py
+++ custom/k8s-scaler/app/main.py
@@ -1,13 +1,35 @@
+from kubernetes import client, config
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+import base64
 import json
 
 app = FastAPI()
+config.load_incluster_config()
 
-@app.post("/mutate/pod_limits")
-async def mutate_pod_limits(request: Request):
-    response = {
-        "success": True
-    }
+k8s_v1 = client.CoreV1Api()
 
-    return JSONResponse(content=response)
+@app.post('/mutate/pod_limits')
+async def set_pod_limits_creation(request: Request):
+    content = request.get_json()
+    pod = content['request']['object']
+    container = pod['spec']['containers'][0]
+    deployment_name = pod['metadata']['labels']['app']
+
+    cpu = k8s_v1.read_namespaced_config_map(name='cpu-limits', namespace='scaler').data[deployment_name]
+    mem = k8s_v1.read_namespaced_config_map(name='mem-limits', namespace='scaler').data[deployment_name]
+
+    new_limits = [
+        {'op': 'add', 'path': '/spec/containers/0/resources/limits/memory', 'value': cpu},
+        {'op': 'add', 'path': '/spec/containers/0/resources/limits/cpu', 'value': mem}
+    ]
+
+    # https://kubernetes.io/docs/reference/access-authn-authz/extensible-admission-controllers
+    return JSONResponse(content = {
+        'response': {
+            'uid': content['request']['id'],
+            'allowed': True,
+            'patchType': 'JSONPatch',
+            'patch': base64.b64encode(json.dumps(new_limits).encode()).decode()
+        }
+    })
