ff860a78c21cfbbb930ed951b3e9f56a725f6a9a
Andy Phan <aphan1@student.unimelb.edu.au>
2025-06-03 01:27:15 +0000

    Tune workload params

--- custom/workload-test/run.sh
+++ custom/workload-test/run.sh
@@ -2,6 +2,6 @@
 
 date > start_time.txt
 
-locust -f locust.py --headless --csv loadtest --csv-full-history --processes=6
+locust -f locust.py --headless --csv loadtest --csv-full-history --processes=7
 
 date > end_time.txt
